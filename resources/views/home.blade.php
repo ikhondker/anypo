b8fd16d610c28f9770f4827fc8d3bce7c18220f2
Iqbal Khondker <khondker@gmail.com>
2023-12-12 18:52:33 +0600

    Interim Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-6 col-lg-5">
               <!-- Heading -->
               <div class="mb-5">
-                <h1>Build awesome apps with your web skills 1</h1>
+                <h1>Build awesome apps with your web skills 2</h1>
                 <p>Built on standard web technology, teams use Front to build beautiful cross-platform hybrid apps in a fraction of the time.</p>
               </div>
               <!-- End Title & Description -->
