803d0c9d7bc71a522ee574dd907ceb24b38321bb
Iqbal Khondker <khondker@gmail.com>
2024-04-23 01:06:47 +0600

    Interim Commit

--- resources/views/components/tenant/actions/hierarchy-actions.blade.php
+++ resources/views/components/tenant/actions/hierarchy-actions.blade.php
@@ -4,24 +4,24 @@
 	 </a>
 	<div class="dropdown-menu dropdown-menu-end">
 		
-		<a class="dropdown-item" href="{{ route('setups.show', $hierarchy->id) }}"><i class="align-middle me-1" data-feather="eye"></i> View Hierarchy</a>
-		<a class="dropdown-item" href="{{ route('setups.edit', $hierarchy->id) }}"><i class="align-middle me-1" data-feather="edit"></i> Edit Hierarchy</a>
+		<a class="dropdown-item" href="{{ route('hierarchies.show', $hierarchy->id) }}"><i class="align-middle me-1" data-feather="eye"></i> View Hierarchy</a>
+		<a class="dropdown-item" href="{{ route('hierarchies.edit', $hierarchy->id) }}"><i class="align-middle me-1" data-feather="edit"></i> Edit Hierarchy</a>
 
 		@can('create', App\Models\Tenant\Workflow\Hierarchy::class)
 			<div class="dropdown-divider"></div>
-			<a class="dropdown-item" href="{{ route('items.create') }}"><i class="align-middle me-1" data-feather="plus-circle"></i> Create Hierarchy</a>
+			<a class="dropdown-item" href="{{ route('hierarchies.create') }}"><i class="align-middle me-1" data-feather="plus-circle"></i> Create Hierarchy</a>
 		@endcan
 
 		
 		@can('delete', $hierarchy)
 			<div class="dropdown-divider"></div>
 			@if ($hierarchy->enable)
-				<a class="dropdown-item sw2-advance" href="{{ route('items.destroy', $hierarchy->id) }}"
+				<a class="dropdown-item sw2-advance" href="{{ route('hierarchies.destroy', $hierarchy->id) }}"
 					data-entity="Hierarchy" data-name="{{ $hierarchy->name }}" data-status="Disable"
 					data-bs-toggle="tooltip" data-bs-placement="top" title="Disable Hierarchy">
 					<i class="align-middle me-1 text-danger" data-feather="bell-off"></i> Disable Hierarchy</a>
 			@else
-				<a class="dropdown-item sw2-advance" href="{{ route('items.destroy', $hierarchy->id) }}"
+				<a class="dropdown-item sw2-advance" href="{{ route('hierarchies.destroy', $hierarchy->id) }}"
 					data-entity="Hierarchy" data-name="{{ $hierarchy->name }}" data-status="Enable"
 					data-bs-toggle="tooltip" data-bs-placement="top" title="Enable Hierarchy">
 					<i class="align-middle me-1 text-success" data-feather="bell"></i> Enable Hierarchy</a>
