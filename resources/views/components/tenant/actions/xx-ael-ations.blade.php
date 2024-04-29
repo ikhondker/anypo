3a0cf40dc6699561de001474e70cb19eeb912a94
Iqbal Khondker <khondker@gmail.com>
2024-04-30 00:20:14 +0600

    Interim Commit

--- resources/views/components/tenant/actions/xx-ael-ations.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="dropdown me-2 d-inline-block position-relative">
-	<a class="btn btn-light bg-white shadow-sm dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-display="static">
-		<i class="align-middle mt-n1 text-danger" data-feather="folder"></i> Ael Actions
-	 </a>
-	<div class="dropdown-menu dropdown-menu-end">
-		<a class="dropdown-item" href="{{ route('ael.manual') }}"><i class="align-middle me-1" data-feather="eye"></i> Manual Accounting (*)</a>
-		<a class="dropdown-item" href="{{ route('ael.index') }}"><i class="align-middle me-1" data-feather="eye"></i> Manual Accounting (*)</a>
-	</div>
-</div>
