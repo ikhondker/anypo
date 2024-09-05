799275c71100fad6d55f2d6176d4119958680561
Iqbal Khondker <khondker@gmail.com>
2024-09-06 00:29:59 +0600

    Intermediate

--- resources/views/components/tenant/buttons/xxshow/xxedit.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-	<th>&nbsp;</th>
-	<td>
-		<a href="{{ route($route.'.edit',$id) }}" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
-			<i class="align-middle" data-lucide="edit"></i> Edit </a>
-	</td>
-</tr>
